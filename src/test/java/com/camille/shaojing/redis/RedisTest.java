5d69e9448c1210f72a2c7257bca32e873fc39bb4
'lichaozhong' <'569080580@qq.com'>
2018-01-22 12:26:21 +0800

    gitignore;

--- src/test/java/com/camille/shaojing/redis/RedisTest.java
+++ src/test/java/com/camille/shaojing/redis/RedisTest.java
@@ -6,10 +6,10 @@ public class RedisTest {
 	public static void main(String[] args) {
 		Jedis jedis = new Jedis("192.168.1.70", 6379); // 创建客户端 设置IP和端口
 		jedis.auth("Camille2.7");
-		jedis.set("boy","lichaozhong"); 
-		jedis.set("girl","shaojing"); // 设置值
-		String boyName=jedis.get("boy");
-		String girlName=jedis.get("girl"); // 获取值
+		jedis.set("boyName","lichaozhong"); 
+		jedis.set("girlName","shaojing"); // 设置值
+		String boyName=jedis.get("boyName");
+		String girlName=jedis.get("girlName"); // 获取值
 		System.out.println("boyName:"+boyName);
 		System.out.println("girlName:"+girlName);
 		jedis.close(); // 释放连接资源
