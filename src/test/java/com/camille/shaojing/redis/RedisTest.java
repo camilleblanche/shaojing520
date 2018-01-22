e0332ecc3214df5fc3f70e40bbf6896b99258c76
'lichaozhong' <'569080580@qq.com'>
2018-01-22 17:39:51 +0800

    RedisUtils junit test

--- src/test/java/com/camille/shaojing/redis/RedisTest.java
+++ src/test/java/com/camille/shaojing/redis/RedisTest.java
@@ -1,9 +1,25 @@
 package com.camille.shaojing.redis;
 
-import redis.clients.jedis.Jedis;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+//import org.springframework.test.context.ContextConfiguration;
 
+import com.camille.shaojing.util.RedisUtils;
+
+import redis.clients.jedis.Jedis;
+//@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/config/spring/spring-redis.xml"})
 public class RedisTest {
-	public static void main(String[] args) {
+	private RedisUtils redisUtils;
+	@Before
+	public void before() {
+		@SuppressWarnings("resource")
+		ApplicationContext context=new FileSystemXmlApplicationContext("src/main/webapp/WEB-INF/config/spring/spring-redis.xml");  
+		redisUtils=context.getBean("redisUtil",RedisUtils.class);  
+	}
+	@Test
+	public void simpleJedisTest() {
 		Jedis jedis = new Jedis("192.168.1.70", 6379); // 创建客户端 设置IP和端口
 		jedis.auth("Camille2.7");
 		jedis.set("boyName","lichaozhong"); 
@@ -13,5 +29,9 @@ public class RedisTest {
 		System.out.println("boyName:"+boyName);
 		System.out.println("girlName:"+girlName);
 		jedis.close(); // 释放连接资源
+	}
+	@Test
+	public void jedisUtilsTest() {
+		redisUtils.get("wife");
 	}
 }
