517167777944f826a4617236d2b4ed95319040d3
'lichaozhong' <'569080580@qq.com'>
2018-03-29 14:20:27 +0800

    deep clone.

--- src/main/java/com/camille/shaojing/util/BaseUtils.java
+++ src/main/java/com/camille/shaojing/util/BaseUtils.java
@@ -1,5 +1,11 @@
 package com.camille.shaojing.util;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Date;
+
 public class BaseUtils {
 	/**
 	 * 判空
@@ -13,4 +19,36 @@ public class BaseUtils {
 		}
 		return rtnFlag;
 	}
+	
+	/**
+	 * 获取随机数
+	 * @param num
+	 * @return
+	 */
+	public static long getRandomNumber(int num){
+		//String str= DateUtils.getStrByDate(new Date(),"yyyyMMddHHmmss");
+		String str= DateUtils.getStrByDate(new Date(),"yyyyMMddHHmm");
+		for(int i=0;i<num;i++){//随机得到num个0到10的数字
+			str=str+(int)(Math.random()*10);
+		}
+		return Long.valueOf(str);
+	}
+	
+	@SuppressWarnings("unchecked")
+	/**
+	 * 深度克隆
+	 * @param obj
+	 * @return
+	 * @throws Exception
+	 */
+	public static <T> T clone(T obj) throws Exception {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(baos);
+        oos.writeObject(obj);
+        // 说明：调用ByteArrayInputStream或ByteArrayOutputStream对象的close方法没有任何意义
+        // 这两个基于内存的流只要垃圾回收器清理对象就能够释放资源，这一点不同于对外部资源（如文件流）的释放
+        ByteArrayInputStream bin = new ByteArrayInputStream(baos.toByteArray());
+        ObjectInputStream ois = new ObjectInputStream(bin);
+        return (T)ois.readObject();
+    }
 }
